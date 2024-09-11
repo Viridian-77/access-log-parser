3786477574b4e22110e23cdb5303ffc25845a65f
Petr Popov <craftvector@yandex.ru>
2024-09-11 14:34:59 +0300

    changed logic to calculate arithmetic operations

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int y = new Scanner(System.in).nextInt();
+        int sum = x + y;
+        int diff = x - y;
+        int product = x * y;
+        double quotient = (double) x / y;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + diff);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
     }
 }
