608a947e9eeac6ac0e01fa84b14ac60d24103684
Petr Popov <craftvector@yandex.ru>
2024-12-02 20:28:38 +0300

    added method getFileLinesInfo and InappropriateLineLengthException

---
+++ src/InappropriateLineLengthException.java
@@ -0,0 +1,17 @@
+public class InappropriateLineLengthException extends RuntimeException {
+    public InappropriateLineLengthException() {
+        super();
+    }
+
+    public InappropriateLineLengthException(String message) {
+        super(message);
+    }
+
+    public InappropriateLineLengthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InappropriateLineLengthException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +17,38 @@ public class Main {
                 count++;
                 System.out.printf("Путь указан верно. Это файл номер %d%n", count);
             }
+            try {
+                getFileLinesInfo(path);
+            } catch (IOException | InappropriateLineLengthException ex) {
+                ex.printStackTrace();
+            }
         }
+    }
+
+    public static void getFileLinesInfo(String path) throws IOException {
+        int countLine = 0;
+        int minLineLength = Integer.MAX_VALUE;
+        int maxLineLength = 0;
+        try (FileReader fileReader = new FileReader(path);
+             BufferedReader reader =
+                     new BufferedReader(fileReader);) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int length = line.length();
+                if (length > 1024) {
+                    throw new InappropriateLineLengthException("Строка не должна быть длинее 1024 символов");
+                }
+                countLine++;
+                if (length < minLineLength) {
+                    minLineLength = length;
+                }
+                if (length > maxLineLength) {
+                    maxLineLength = length;
+                }
+            }
+        }
+        System.out.println("Общее количество строк в файле: " + countLine);
+        System.out.println("Длина самой длинной строки в файле: " + maxLineLength);
+        System.out.println("Длина самой короткой строки в файле: " + minLineLength);
     }
 }
